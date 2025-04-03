96f3f17cfcf715a9e8c468b0029e7dd50cfc568c
hmlipkin <59354340+hmlipkin@users.noreply.github.com>
2025-04-03 15:22:20 -0400

    Build fix

--- app/api/league/delete/route.ts
+++ app/api/league/delete/route.ts
@@ -1,6 +1,7 @@
 import deleteLeague from "@/actions/deleteLeague";
-
+import { NextResponse } from "next/server";
 export const POST = async (request: Request) => {
   const { leagueId } = await request.json();
-  return await deleteLeague(leagueId);
+  await deleteLeague(leagueId);
+  return NextResponse.json({ message: "League deleted" });
 };
