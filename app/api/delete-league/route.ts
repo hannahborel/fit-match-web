fa16ccbb15f4df35562417cfd1a9cc2e3ea7ebf1
hannahborel <hannahborel@icloud.com>
2025-07-19 14:43:15 -0500

    ISSUE FIX - delete-league: Leagues were not being deleted but the response was sucessfull. Solution: updated action peramiter to work with data.id

--- app/api/delete-league/route.ts
+++ app/api/delete-league/route.ts
@@ -2,6 +2,6 @@ import deleteLeague from "@/actions/deleteLeague";
 import { NextResponse } from "next/server";
 export const POST = async (request: Request) => {
   const { leagueId } = await request.json();
-  await deleteLeague(leagueId);
+  await deleteLeague({ id: leagueId });
   return NextResponse.json({ message: "League deleted" });
 };
